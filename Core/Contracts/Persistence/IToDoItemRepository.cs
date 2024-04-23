5d18eede0520408ffd34a4ddcdd5584224cbf5f2
bb0b <konlawatit@gmail.com>
2024-04-24 02:45:17 +0700

    add(Core) IRepository

---
+++ Core/Contracts/Persistence/IToDoItemRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Domain.Entities;
+
+namespace Company.ClassLibrary1;
+
+public interface IToDoItemRepository {
+    Task<ToDoItem> CreateToDoItem(ToDoItem toDoItem);
+    Task<List<ToDoItem>> GetToDoItems(int todoListId);
+    Task<ToDoItem> UpdateToDoItem(ToDoItem toDoItem);
+    Task<bool> DeleteToDoItem(int todoItemId);
+}
---
+++ Core/Contracts/Persistence/IToDoListRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Domain.Entities;
+
+namespace Company.ClassLibrary1;
+
+public interface IToDoListRepository {
+    Task<ToDoList> CreateToDoList(ToDoList toDoList);
+    Task<List<ToDoList>> GetToDoLists();
+    Task<List<ToDoList>> UpdateToDoList(ToDoList toDoList);
+    Task<bool> DeletToDoList(int todoListId);
+}
