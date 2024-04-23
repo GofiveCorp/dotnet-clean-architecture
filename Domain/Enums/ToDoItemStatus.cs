ab77f26bc7dc8b55a12abeef28fbf16dc8b7e9f2
bb0b <konlawatit@gmail.com>
2024-04-24 02:29:08 +0700

    add(Domain): ToDoItemStatus, TODoListStatus

---
+++ Domain/Enums/ToDoItemStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace Company.ClassLibrary1;
+
+public enum ToDoItemStatus {
+    Success = 12001,
+    InProgress = 12002,
+    Cancel = 12003,
+
+}
---
+++ Domain/Enums/ToDoListStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace Company.ClassLibrary1;
+
+public enum ToDoListStatus {
+    Success = 11001,
+    InProgress = 11002,
+    Cancel = 11003
+}
